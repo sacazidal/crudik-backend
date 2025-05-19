0f3f74147c7f8bf85d084fbd441a382a18866ad6
sacazidal <tw1nkle001@yandex.ru>
2025-05-20 00:21:13 +0700

    инициализация фастифи, корс, шлем, регистрация роута. запуск сервера

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import cors from "@fastify/cors";
+import fastify from "fastify";
+import todoRoutes from "./routes/todo.route.js";
+import helmet from "@fastify/helmet";
+import { config } from "dotenv";
+
+config();
+
+const app = fastify();
+
+app.register(cors, {
+  origin: [process.env.CORS_ORIGIN!, process.env.CORS_LOCALHOST!],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
+app.register(helmet, {
+  crossOriginResourcePolicy: { policy: "same-site" },
+});
+await app.register(todoRoutes, { prefix: "/api" });
+
+app.addHook("onRequest", (req, reply, done) => {
+  console.log(`[${req.method}] ${req.url}`);
+  console.log("=> Headers:", req.headers);
+  console.log("=> Response headers:", reply.getHeaders());
+  done();
+});
+
+export { app };
